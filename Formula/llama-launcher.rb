fe8e5c26ab4eb5552b1ac86b42ba524d932767bf
Airic Lenz <airic@airiclenz.com>
2026-05-20 13:30:56 +0200

    Add llama-launcher formula v1.1.0

---
+++ Formula/llama-launcher.rb
@@ -0,0 +1,18 @@
+class LlamaLauncher < Formula
+  desc "CLI tool for managing local LLM servers (llama.cpp, Ollama, LM Studio)"
+  homepage "https://github.com/airiclenz/llama-launcher"
+  url "https://github.com/airiclenz/llama-launcher/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "211dcb85a9446f401c221e98720a419183b04929459f9b40bbc362f014d813ee"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-X github.com/airiclenz/llama-launcher/internal/launcher.Version=#{version}"
+    system "go", "build", *std_go_args(ldflags:)
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/llama-launcher version")
+  end
+end
