d1cff9de711aa88ee0ea8f4f8c3a420402b7297d
Airic Lenz <airic@airiclenz.com>
2026-08-25 18:37:56 +0200

    Update apogee to 0.17.1

--- Formula/apogee.rb
+++ Formula/apogee.rb
@@ -1,7 +1,7 @@
 class Apogee < Formula
   desc "Terminal coding agent built for smaller local models, better with bigger ones"
   homepage "https://github.com/airiclenz/apogee"
-  version "0.17.0"
+  version "0.17.1"
   license "MIT"
 
   # Installs the prebuilt binary for this platform rather than compiling, so no Go
@@ -10,23 +10,23 @@ class Apogee < Formula
   # `make dist` in the project repo.
   on_macos do
     on_arm do
-      url "https://github.com/airiclenz/apogee/releases/download/v0.17.0/apogee_0.17.0_darwin_arm64.tar.gz"
-      sha256 "d044f8eb93d4f3af907ba1cc599b7531aecb711d5e50f5eb37d4cf67ffeeb7ee"
+      url "https://github.com/airiclenz/apogee/releases/download/v0.17.1/apogee_0.17.1_darwin_arm64.tar.gz"
+      sha256 "8420ccd28dd97fce2c6ca5f5d2befc796503a7a9b1458b953a0801423073260b"
     end
     on_intel do
-      url "https://github.com/airiclenz/apogee/releases/download/v0.17.0/apogee_0.17.0_darwin_amd64.tar.gz"
-      sha256 "dd8790c9dc452f1210b6eaa61bdbaf79e4f0e0eb9cb58fa5775614d6c13e9f65"
+      url "https://github.com/airiclenz/apogee/releases/download/v0.17.1/apogee_0.17.1_darwin_amd64.tar.gz"
+      sha256 "d4283f4baf9438bc5c5bce0f8c01b0ad5e5eb69fe67d91aa55bbad7452c6c20f"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/airiclenz/apogee/releases/download/v0.17.0/apogee_0.17.0_linux_arm64.tar.gz"
-      sha256 "5e6ffae1903987681f94b778fe4f3a0358ebded150ec396e2aedd985b0b224e3"
+      url "https://github.com/airiclenz/apogee/releases/download/v0.17.1/apogee_0.17.1_linux_arm64.tar.gz"
+      sha256 "0b4b9c5adc89e174a5851afdca20dc3b8e6eec0f393754c01b60d90f16d406e0"
     end
     on_intel do
-      url "https://github.com/airiclenz/apogee/releases/download/v0.17.0/apogee_0.17.0_linux_amd64.tar.gz"
-      sha256 "1366aef8e158f2a20f0dbc87b6a531cebb26c2c1cf95beffdf5615e2a6e30e9b"
+      url "https://github.com/airiclenz/apogee/releases/download/v0.17.1/apogee_0.17.1_linux_amd64.tar.gz"
+      sha256 "5796c9497807c17139682272559bb9e141179291eafeab8f9885c2ece1a9c196"
     end
   end
 
