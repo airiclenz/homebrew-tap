7422425e3fabd3b6c35686a33e34ac362c7cbe9e
Airic Lenz <airic@airiclenz.com>
2026-08-27 16:56:34 +0200

    Update apogee to 0.18.0

--- Formula/apogee.rb
+++ Formula/apogee.rb
@@ -1,7 +1,7 @@
 class Apogee < Formula
   desc "Terminal coding agent built for smaller local models, better with bigger ones"
   homepage "https://github.com/airiclenz/apogee"
-  version "0.17.1"
+  version "0.18.0"
   license "MIT"
 
   # Installs the prebuilt binary for this platform rather than compiling, so no Go
@@ -10,23 +10,23 @@ class Apogee < Formula
   # `make dist` in the project repo.
   on_macos do
     on_arm do
-      url "https://github.com/airiclenz/apogee/releases/download/v0.17.1/apogee_0.17.1_darwin_arm64.tar.gz"
-      sha256 "8420ccd28dd97fce2c6ca5f5d2befc796503a7a9b1458b953a0801423073260b"
+      url "https://github.com/airiclenz/apogee/releases/download/v0.18.0/apogee_0.18.0_darwin_arm64.tar.gz"
+      sha256 "67a6dbdbacbfbf7a228b4d792ab323e2f00e8caade2b0c0a12241c9018bb501a"
     end
     on_intel do
-      url "https://github.com/airiclenz/apogee/releases/download/v0.17.1/apogee_0.17.1_darwin_amd64.tar.gz"
-      sha256 "d4283f4baf9438bc5c5bce0f8c01b0ad5e5eb69fe67d91aa55bbad7452c6c20f"
+      url "https://github.com/airiclenz/apogee/releases/download/v0.18.0/apogee_0.18.0_darwin_amd64.tar.gz"
+      sha256 "211229dc1e1ca326fb7e8480b9664f53c790a381eb3fa1fa44cc554069d28629"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/airiclenz/apogee/releases/download/v0.17.1/apogee_0.17.1_linux_arm64.tar.gz"
-      sha256 "0b4b9c5adc89e174a5851afdca20dc3b8e6eec0f393754c01b60d90f16d406e0"
+      url "https://github.com/airiclenz/apogee/releases/download/v0.18.0/apogee_0.18.0_linux_arm64.tar.gz"
+      sha256 "6e87533231a087aec175d1cd8043d22ab319f7d432ccee40df3fe71f9a502489"
     end
     on_intel do
-      url "https://github.com/airiclenz/apogee/releases/download/v0.17.1/apogee_0.17.1_linux_amd64.tar.gz"
-      sha256 "5796c9497807c17139682272559bb9e141179291eafeab8f9885c2ece1a9c196"
+      url "https://github.com/airiclenz/apogee/releases/download/v0.18.0/apogee_0.18.0_linux_amd64.tar.gz"
+      sha256 "5413f675d3dd1874ccbcad2b3b64e13317954ba8999fe261d32dc468c6fa8144"
     end
   end
 
